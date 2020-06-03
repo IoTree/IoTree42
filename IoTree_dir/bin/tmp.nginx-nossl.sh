b97cdb52aa954acddf59b5226c70b1a194944d2c
Sebastian Stadler <50913382+IoTree@users.noreply.github.com>
2020-06-03 19:33:46 +0200

    Update tmp.nginx-nossl.sh

--- IoTree_dir/bin/tmp.nginx-nossl.sh
+++ IoTree_dir/bin/tmp.nginx-nossl.sh
@@ -25,6 +25,10 @@ server {
         location /static/ {
                 root /home/$USERNAME/dj_iot;
         }
+        
+        location /media/ {
+                root /home/$USERNAME/dj_iot;
+        }
 
         location / {
                 include proxy_params;
