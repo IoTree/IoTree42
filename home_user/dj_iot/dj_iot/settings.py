c0c7d0245fd5206809826f15cdb211e0808a597f
BioMed of Munich University <50913382+IoTree@users.noreply.github.com>
2022-08-18 18:58:32 +0200

    bug fix django -v > 4.0

--- home_user/dj_iot/dj_iot/settings.py
+++ home_user/dj_iot/dj_iot/settings.py
@@ -117,9 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-    {
-        'NAME': 'users.validators.MinimumLengthhValidator',
-    },
 ]
 
 
